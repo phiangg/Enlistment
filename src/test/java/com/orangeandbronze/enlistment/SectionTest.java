4a2f78aab3891a588cf2e7d1518f9cc53a34c0f5
phiangg <sophiaquizon1102@gmail.com>
2023-05-09 06:05:56 +0800

    Updated the SectionTest

--- src/test/java/com/orangeandbronze/enlistment/SectionTest.java
+++ src/test/java/com/orangeandbronze/enlistment/SectionTest.java
@@ -6,6 +6,7 @@ import java.time.LocalTime;
 
 import static com.orangeandbronze.enlistment.Period.*;
 import static com.orangeandbronze.enlistment.Days.*;
+import static com.orangeandbronze.enlistment.SubjectType.LECTURE;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -15,21 +16,23 @@ public class SectionTest {
     private LocalTime endTime;
     private final Schedule MTH_H0830 = new Schedule(MTH, H0830, startTime, endTime);
     private final Schedule MTH_H1300 = new Schedule(MTH, H1300, startTime, endTime);
-    private final Room ROOM_101 = new Room("101", 50);
-    private final Room ROOM_102 = new Room("102", 60);
+    final Room ROOM101_5 = new Room("ROOM101", 5);
+    final Room ROOM201_5 = new Room("ROOM201", 5);
+    final Subject MATH101_3_LECTURE = new Subject("MATH101", 3, LECTURE);
+    final Subject MATH201_2_LECTURE = new Subject("MATH201", 2, LECTURE);
 
     @Test
     public void testCanShareRoomWithSameRoom() {
-        Section section1 = new Section("Math", MTH_H0830, ROOM_101, Subject.NONE);
-        Section section2 = new Section("Science", MTH_H1300, ROOM_101, Subject.NONE);
+        Section section1 = new Section("Math", MTH_H0830, ROOM101_5, MATH101_3_LECTURE);
+        Section section2 = new Section("Science", MTH_H1300, ROOM101_5, MATH101_3_LECTURE);
 
         assertTrue(section1.canShareRoomWith(section2));
     }
 
     @Test
     public void testCannotShareRoomWithDifferentRoom() {
-        Section section1 = new Section("Math", MTH_H0830, ROOM_101, Subject.NONE);
-        Section section2 = new Section("Science", MTH_H1300, ROOM_102, Subject.NONE);
+        Section section1 = new Section("Math", MTH_H0830, ROOM101_5, MATH101_3_LECTURE);
+        Section section2 = new Section("Science", MTH_H1300, ROOM201_5, MATH201_2_LECTURE);
 
         assertFalse(section1.canShareRoomWith(section2));
     }
