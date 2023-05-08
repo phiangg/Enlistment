a2981c8b158998ae173ba80e55be6fb4ca6cccb2
Mendeir <adriannemagracia.w@gmail.com>
2023-05-08 10:56:02 +0800

    Implement Section to be identifiable by sectionID

--- src/main/java/com/orangeandbronze/enlistment/Section.java
+++ src/main/java/com/orangeandbronze/enlistment/Section.java
@@ -1,5 +1,35 @@
 package com.orangeandbronze.enlistment;
 
 class Section {
+    private final String sectionID;
 
+    Section(String sectionID) {
+        if (sectionID == null)
+            throw new NullPointerException("Section ID cannot be null");
+
+        if (sectionID.trim().equals(""))
+            throw new IllegalArgumentException("Section was empty or whitespace");
+
+        this.sectionID = sectionID;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Section section = (Section) o;
+
+        return sectionID.equals(section.sectionID);
+    }
+
+    @Override
+    public int hashCode() {
+        return sectionID.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return sectionID;
+    }
 }
