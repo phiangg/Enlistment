4ca291999b53412c1135dc32b1f1c8e86973b50e
Mendeir <adriannemagracia.w@gmail.com>
2023-05-08 16:34:40 +0800

    Add subject in section

--- src/main/java/com/orangeandbronze/enlistment/Section.java
+++ src/main/java/com/orangeandbronze/enlistment/Section.java
@@ -8,18 +8,21 @@ class Section {
     private final Schedule schedule;
     private final Room room;
     private final Instructor instructor;
+    private final Subject subject;
 
-    Section(String sectionID, Schedule schedule, Room room, Instructor instructor) {
+    Section(String sectionID, Schedule schedule, Room room, Instructor instructor, Subject subject) {
 
         notBlank(sectionID, "Section ID cannot be null, empty, or whitespace");
         isTrue(isAlphanumeric(sectionID), "Section ID must be alphanumeric, was: " + sectionID);
         notNull(schedule);
         notNull(room);
+        notNull(subject);
 
         this.sectionID = sectionID;
         this.schedule = schedule;
         this.room = room;
         this.instructor = instructor;
+        this.subject = subject;
     }
 
     void checkForConflict(Section other) {
